192ac8a54b04d97ebd61057779f6d1d7cd244430
Wind <:lizy@ibaiminet.com  BAImi360git config --global user.email >
2019-04-24 13:26:58 +0800

    add delete function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import './style.css'
 class TodoList extends Component{
   constructor(props){
     super(props)
@@ -11,13 +12,13 @@ class TodoList extends Component{
     return (
       <Fragment>
         <div>
-          <input type="text" value = {this.state.inputVal} onChange={this.handleInput.bind(this)}/>
-          <button onClick = { this.handleSubmit.bind(this) }>提交</button>
+          <input className="input" type="text" value = {this.state.inputVal} onChange={this.handleInput.bind(this)}/>
+          <button className="button" onClick = { this.handleSubmit.bind(this) }>提交</button>
         </div>
         <ul>
           {
             this.state.list.map((item, index) => {
-              return <li>{index}-{item}</li>
+              return <li key={index} onClick={ this.handleDelete.bind(this, index) }>{item}</li>
             })
           }
         </ul>
@@ -29,10 +30,20 @@ class TodoList extends Component{
     this.setState({
       inputVal: e.target.value
     })
+
   }
   handleSubmit(){
     this.setState({
-      list: [...this.state.list, this.state.inputVal]
+      list: [...this.state.list, this.state.inputVal],
+      inputVal: ''
+    })
+  }
+  handleDelete(index){
+    console.log(index)
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list
     })
   }
 }
