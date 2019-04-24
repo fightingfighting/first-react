2b4897fd5ee5941b8ccc83c17b6fd5924df99f31
Wind <:lizy@ibaiminet.com  BAImi360git config --global user.email >
2019-04-24 09:32:01 +0800

    practice todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,39 @@
-import React, {Component} from 'react'
+import React, { Component, Fragment } from 'react'
 class TodoList extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      inputVal: '',
+      list: []
+    }
+  }
   render(){
     return (
-      <div>hello world</div>
+      <Fragment>
+        <div>
+          <input type="text" value = {this.state.inputVal} onChange={this.handleInput.bind(this)}/>
+          <button onClick = { this.handleSubmit.bind(this) }>提交</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return <li>{index}-{item}</li>
+            })
+          }
+        </ul>
+      </Fragment>
     )
+  }
+
+  handleInput(e){
+    this.setState({
+      inputVal: e.target.value
+    })
+  }
+  handleSubmit(){
+    this.setState({
+      list: [...this.state.list, this.state.inputVal]
+    })
   }
 }
 
